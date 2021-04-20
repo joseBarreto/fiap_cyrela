5fcefc03f7f33ba65ba6f362cc020bd724477b57
José Barreto Teixeira de Paula <jose.paula@luizalabs.com>
2021-04-19 22:39:33 -0300

    add redirect home

--- CyrelaServices/Controllers/HealthCheckController.cs
+++ CyrelaServices/Controllers/HealthCheckController.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace CyrelaServices.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class HealthCheckController : ControllerBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IActionResult Index()
         {
--- CyrelaServices/Controllers/HomeController.cs
+++ CyrelaServices/Controllers/HomeController.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace CyrelaServices.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class HomeController : ControllerBase
+    /// <summary>
+    /// 
+    /// </summary>
+    [Route("")]
+    [Route("Home")]
+    [Route("Home/Index")]
+    [Route("Home/Index.html")]
+    [Route("Index")]
+    [Route("Index.html")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HomeController : Controller
     {
         /// <summary>
         /// 
